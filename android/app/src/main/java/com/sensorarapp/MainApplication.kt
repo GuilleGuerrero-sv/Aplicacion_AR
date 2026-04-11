034bd85be3e49d3911947b2800a79a33262acc40
Ander2211 <anderhdz06@gmail.com>
2026-04-10 20:50:22 -0600

    boton de AR agregado que redirecciona a la camara

--- android/app/src/main/java/com/sensorarapp/MainApplication.kt
+++ android/app/src/main/java/com/sensorarapp/MainApplication.kt
@@ -19,7 +19,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              add(com.viromedia.bridge.ReactViroPackage(com.viromedia.bridge.ReactViroPackage.ViroPlatform.valueOf("AR")))
             }
 
         override fun getJSMainModuleName(): String = "index"
